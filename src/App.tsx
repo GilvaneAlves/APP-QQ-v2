4d3bb207d0c2a94be265ce34c01433003d640b29
Gilvane Alves Dias <gilwanealwes@gmail.com>
2026-05-05 19:05:57 -0300

    chore: configurar linting e formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>APP-QQ v2</h1>
-      <p>Scanner de Códigos de Barras - PWA</p>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind funcionando 🚀
+      </h1>
     </div>
   )
 }
-
-export default App
